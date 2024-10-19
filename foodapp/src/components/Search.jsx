537e8d1a3442da7a272230576a9a13559564d987
orewagenki <orewagenki3@gmail.com>
2024-10-19 12:10:15 -0600

    Displaying images and titles of food items and styled the Nav bar.

--- foodapp/src/components/Search.jsx
+++ foodapp/src/components/Search.jsx
@@ -3,17 +3,17 @@ import { useEffect, useState } from "react";
 const URL = "https://api.spoonacular.com/recipes/complexSearch";
 const API_KEY = "aa9dfa118e82448dae1e063ea40116ae";
 
-export default function Search() {
+export default function Search({ foodData, setFoodData }) {
   const [query, setQuery] = useState("pizza");
 
   //syntax of the useEffect hook
   useEffect(() => {
     async function fetchFood() {
       const res = await fetch(`${URL}?query=${query}&apiKey=${API_KEY}`);
-      const data = res.json();
+      const data = await res.json();
       console.log(data);
+      setFoodData(data.results);
     }
-
     fetchFood();
   }, [query]);
 
